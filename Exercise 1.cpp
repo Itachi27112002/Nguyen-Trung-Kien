6611f99c2bd9d173ae48ea471a734487a87adf1a
Itachi27112002 <nguyentrungkien27112002@gmail.com>
2021-12-25 19:59:08 +0700

    PRACTICE1

---
+++ Exercise 1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int n;
+	printf("Nhap so nguyen n=");
+	scanf("%d",&n);
+	
+	if(n>0){
+		float s=0, i=1;
+		while(i<n){
+			s+=1/i;
+			i++;
+		}
+	printf("tong la: %f",s);
+}else{
+	printf("Errot");
+	}
+}
